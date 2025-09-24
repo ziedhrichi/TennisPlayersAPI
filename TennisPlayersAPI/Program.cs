6d5dfc610948b323670bdfde88e18a8719339769
Zied HRICHI <zhrichi@amaris.com>
2025-09-25 00:02:58 +0200

    Update swagger configuration for prod

--- TennisPlayersAPI/Program.cs
+++ TennisPlayersAPI/Program.cs
@@ -48,16 +48,14 @@ builder.Services.AddScoped<IPlayersService, PlayersService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tennis Player API v1");
-        c.RoutePrefix = string.Empty; // Swagger UI accessible à la racine (http://localhost:5000)
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tennis Player API v1");
+    c.RoutePrefix = string.Empty; // Swagger UI accessible à la racine (http://localhost:5000)
+});
+
 
 app.UseCors("AllowAll");
 
