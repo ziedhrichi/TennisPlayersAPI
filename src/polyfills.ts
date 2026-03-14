3bddc24615e466ae5ac1c12ed19dec12228048fe
Zied HRICHI <dsi.zied.hrichi@gmail.com>
2026-03-14 14:44:51 +0100

    Create src/polyfills.ts file for Angular polyfills

---
+++ src/polyfills.ts
@@ -0,0 +1,15 @@
+// Polyfills for Angular applications
+
+// You can add your own polyfills to this file.
+
+/**
+ * By default, these polyfills are included in Angular CLI. However, if you need to customize them, you can do so below.
+ */
+
+/**
+ * Import all polyfills needed for various functionalities and older browser support
+ */
+
+// Uncomment the following if you need to support IE11
+// import 'core-js/es/reflect';
+// import 'zone.js/dist/zone'; // Included with Angular CLI.
