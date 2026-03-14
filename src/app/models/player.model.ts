f9982ee75745a11dc248da484dbfdcc2f6b08a17
Zied HRICHI <dsi.zied.hrichi@gmail.com>
2026-03-14 14:47:26 +0100

    Create Player model interface

---
+++ src/app/models/player.model.ts
@@ -0,0 +1,10 @@
+export interface Player {
+  id: number;
+  firstName: string;
+  lastName: string;
+  age: number;
+  nationality: string;
+  ranking: number;
+  titles: number;
+  winPercentage: number;
+}
